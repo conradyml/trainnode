16ade162d90c0f71787ae5f708c3fec153151e99
conradyml <conradyml@gmail.com>
2025-01-03 17:57:03 -0500

    first dcc commands added - lights work!

--- index.js
+++ index.js
@@ -36,10 +36,10 @@ socket.on('button', function(toggle) {
 socket.on('throttle',function(target,value) {
 	// target is the REG entry for the locomotive.
 	// value is a 7 bit value for speed. 1st bit is direction, remaning 7 are the 128 speed steps.
-	console.log(" Throttle request received with target:"+target.toString()+" and value:"+buffer.toString())
-	var buffer = Buffer.from([0x3F,value]);
+	console.log(" Throttle request received with target:"+target.toString()+" and value:"+value.toString())
+	var buffer = Buffer.from([0x3F,intToHex(value)]);
 	console.log(" Throttle request submitted to target:"+target.toString()+" and message:"+buffer.toString())
-	sendI2c(target,buffer);
+	sendI2c(intToHex(target),buffer);
 });
 
 socket.on('eStop',function() {
@@ -47,22 +47,21 @@ socket.on('eStop',function() {
 	console.log(" Throttle request received with target:"+target.toString()+" and value:"+buffer.toString())
 	var buffer = Buffer.from([0x3F,0X01]);
 	console.log(" Throttle request submitted to target:"+target.toString()+" and message:"+buffer.toString())
-	sendI2c(target,buffer);
+	sendI2c(intToHex(target),buffer);
 });
 
 socket.on('lights',function(target,value) {
 	// target is the REG entry for the locomotive.
 	// value is true (lights on) or false (lights off)
-	console.log(" Light request received with target:"+target.toString()+" and value:"+buffer.toString())
+	console.log(" Light request received with target:"+target.toString()+" and value:"+value.toString())
 	if(value){
-		var buffer = Buffer.from([0x9F]);
+		var msg = [0x9F]; //on
 		console.log(" Lights on for target:"+target.toString())
-		sendI2c(target,buffer);
 	} else {
-		var buffer = Buffer.from([0x80]);
+		var msg = [0x80]; //off
 		console.log(" Lights off for target:"+target.toString())
-		sendI2c(target,buffer);
-	} 
+	}
+	sendI2c(target,msg);
 });
 
   //Whenever someone disconnects this piece of code is executed
@@ -80,7 +79,7 @@ socket.on('lights',function(target,value) {
 function buttonTouch(msg){
 	io.emit('message',msg);
 	console.log('[' + new Date().toLocaleString('en-us') + '] [on buttonTouch] - '+'sending button push message.');
-	
+
 }
 
 function sendI2c(target,message){
@@ -88,7 +87,11 @@ function sendI2c(target,message){
 // message is a buffer object to send.	
 
 	const i2c1 = i2c.openSync(1);
-    i2c1.writeI2cBlockSync(PIC_ADDR, target, message.length, message);
+	reg = target.toString(16).padStart(2,'0');
+	const buffer = Buffer.from(message);
+    console.log('Sending i2C. Target: '+target+' Message: '+message+ ' Length: '+message.length);
+    console.log('Sending i2c. Target: '+reg+' Message: '+buffer.toString()+' Length: '+buffer.length);
+    i2c1.writeI2cBlockSync(PIC_ADDR,0x03, buffer.length, buffer);
 	i2c1.closeSync();
 }
 
